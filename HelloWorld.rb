f5f430e935bb806750e9e8e29b7061689c2e6e2f
Francisco Ybarhuen <angel@yarkan.pe>
2013-05-14 11:04:31 -0500

    My first ruby auto trainig

---
+++ HelloWorld.rb
@@ -0,0 +1,1 @@
+puts "Hello World!!! (Classical sample =D).";
---
+++ MultiLine.rb
@@ -0,0 +1,6 @@
+puts <<EOF
+    Hi my name is Angel, but many friends of my
+    call me frank. I like software development.
+    My favourites languages are Java, C#, PHP. Today
+    I am testing Ruby and It seems to be great.
+EOF
